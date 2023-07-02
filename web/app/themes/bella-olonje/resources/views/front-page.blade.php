7814c477c91a9e995b2c68f6af6bd9217e911044
lauraspark <tech@sparkgreen.com.au>
2023-07-02 18:00:08 +1000

    [UPDATE] Home banner buttons

--- web/app/themes/bella-olonje/resources/views/front-page.blade.php
+++ web/app/themes/bella-olonje/resources/views/front-page.blade.php
@@ -9,12 +9,20 @@ if( have_rows('flexible_template') ):
 
 <?php if( get_row_layout() == 'home_banner' ): ?>
 
-<div class="container-fluid bg-black">
+<div class="container-fluid bg-black relative">
     <?php $image = get_sub_field('background_image'); ?>
-    <img class="h-screen opacity-50 w-full object-contain" src="<?php echo esc_url($image['url']); ?>"
+    <img class="h-screen opacity-50 w-full object-cover" src="<?php echo esc_url($image['url']); ?>"
         alt="<?php echo $image['alt']; ?>">
+    <div class="absolute top-0 text-white banner-text text-center">
+        <?php the_sub_field('text'); ?>
+        <button class="orange-button">Playstore</button>
+        <button class="white-button ml-5">App store</button>
+    </div>
+    <?php $overlay = get_sub_field('overlay_image'); ?>
 </div>
-
+<div class="overlay-image">
+    <img class="" src="<?php echo esc_url($overlay['url']); ?>" alt="<?php echo $overlay['alt']; ?>">
+</div>
 <?php endif; ?>
 
 <?php endwhile;
